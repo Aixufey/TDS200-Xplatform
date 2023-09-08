e43f1a62498b7e74e527ae44bae4b8acc7a6ca72
Rinseo <jack_11728@msn.com>
2023-09-08 15:36:59 +0200

    Update CounterPage

--- pages/counterPage/CounterPage.tsx
+++ pages/counterPage/CounterPage.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 import {
     View,
     Text,
+    TouchableOpacity,
 } from 'react-native'
+import { useState } from 'react';
+
 
 
 
 
 
 const CounterPage: React.FC = () => {
+
+    const [count, setCount] = useState<number>(0)
+
+    const handleClick = () => {
+        setCount(count + 1)
+    }
+
     return (
-        <View>
+        <View style={{ flexGrow: 1, flexShrink: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>This is a counter page</Text>
+            <Text>{count}</Text>
+            <TouchableOpacity style={{marginTop: 20}} onPress={handleClick}>
+                <Text>Click Me</Text>
+            </TouchableOpacity>
         </View>
     )
 }
